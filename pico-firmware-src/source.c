d0f6f14d3def547579834bf2f9428c45f90ed4a3
Caleb Kemere <ckemere@gmail.com>
2023-08-28 12:38:54 -0500

    Updated firmware code so that LED turns on as soon as we're powered and running

--- pico-firmware-src/source.c
+++ pico-firmware-src/source.c
@@ -16,14 +16,17 @@ int main() {
     gpio_set_dir(LED_PIN, GPIO_OUT);
     gpio_init(OUT_PIN);
     gpio_set_dir(OUT_PIN, GPIO_OUT);
+    gpio_init(MIRROR_PIN);
+    gpio_set_dir(MIRROR_PIN, GPIO_OUT);
     gpio_init(IN_PIN);
     gpio_set_dir(IN_PIN, GPIO_IN);
 
     uint state = 0;
 
+    gpio_put(LED_PIN, 1);
+
     while (true) {
         if ((gpio_get(IN_PIN) == 1) && (state == 0)) {
-            gpio_put(LED_PIN, 1);
             gpio_put(MIRROR_PIN, 1);
             gpio_put(OUT_PIN, 1);
             sleep_us(100);
@@ -35,7 +38,6 @@ int main() {
             state = 1;
         }
         else if (gpio_get(IN_PIN) == 0) {
-            gpio_put(LED_PIN, 0);
             gpio_put(OUT_PIN, 0);
             gpio_put(MIRROR_PIN, 0);
             state = 0;
